4bf41b30764048d48ad5c6c767f2f2d7492ab5d9
Sander Laarhoven <sander@laarhoven.org>
2016-12-15 19:41:35 +0100

    Revamp code structure.

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+/**
+ * Wikipedia Random Browser
+ * By Sander Laarhoven
+ * https://git.io/wikigame
+ * Licensed under the MIT License
+ * Copyright (c) 2016 Sander Laarhoven All Rights Reserved.
+ */
+
+
+/*
+ * Import modules.
+ * ================ */
+
+let Game            = require('./assets/js/game')
+    Game.version    = require(__dirname+'/package.json').version
+
+/*
+ * Define app globals.
+ * ==================== */
+
+let WikiPage
+Game.Settings = {}
+Game.Settings.StartPage = "https://en.wikipedia.org/wiki/Special:Random"
+
+
+/*
+ * Initialize the game.
+ * ===================== */
+
+//Game.LoadConfiguration()
+
+Game.UpdateStatistics()
+
+Game.RegisterEventListeners()
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,62 +1,70 @@
 /**
- * Wikipedia Random Browser v1.0.0
+ * Wikipedia Random Browser
  * By Sander Laarhoven
- * https://github.com/lesander/wikibrowser
- * MIT Licensed (c) 2016
+ * https://git.io/wikigame
+ * Licensed under the MIT License
+ * Copyright (c) 2016 Sander Laarhoven All Rights Reserved.
  */
 
-/* Include modules. */
-const remote = require('electron').remote
-const BrowserWindow = remote.BrowserWindow
-const $ = require('jQuery')
+/*
+ * Import modules.
+ * ================ */
 
-/* Set global empty objects. */
-Game = {}
-let WikiPage
+const remote          = require('electron').remote
+const BrowserWindow   = remote.BrowserWindow
+const $               = require('jQuery')
 
-/* Define game settings. */
-Game.Settings = {
-  RandomPage: "https://en.wikipedia.org/wiki/Special:Random"
+
+/*
+ * Game module.
+ * ============= */
+
+exports = module.exports = {}
+
+
+/**
+ * Register all event listeners of the application.
+ */
+exports.RegisterEventListeners = function () {
+
+  /* Start game on click.  */
+  $("#game-start").on("click", Game.Start)
+
+  /* Toggle settings page on click. */
+  $("#game-config").on("click", Game.ToggleConfig)
+
+  /* Hide Game Over on click. */
+  $("#game-over button#close").on("click", function() {
+    $("section#intro").show()
+    $("section#stats").show()
+    $("section#game-over").hide()
+    $("section#game").hide()
+    $("body").attr("style", "")
+  })
+
+  /* Update StartPage on change. */
+  UseCustomStartPage = false
+  $("input[name=config-enable-custom-startpage]").on("change", function() {
+    if ( $("input[name=config-enable-custom-startpage]").is(":checked") ) {
+      $("input[name=config-value-custom-startpage]").trigger("change")
+      UseCustomStartPage = true
+    } else {
+      UseCustomStartPage = false
+      Game.Settings.StartPage = "https://en.wikipedia.org/wiki/Special:Random"
+    }
+  })
+
+  $("input[name=config-value-custom-startpage]").on("keyup change", function() {
+    if (UseCustomStartPage) Game.Settings.StartPage = $(this).val()
+  })
+
 }
 
-/* Update game statistics. */
-GameUpdateStats()
-
-/* Start game on click.  */
-$("#game-start").on("click", GameStart)
-
-/* Toggle settings page on click. */
-$("#game-config").on("click", ToggleConfig)
-
-/* Hide Game Over on click. */
-$("#game-over button#close").on("click", function() {
-  $("section#intro").show()
-  $("section#stats").show()
-  $("section#game-over").hide()
-  $("section#game").hide()
-  $("body").attr("style", "")
-})
-
-/* Update RandomPage on change. */
-UseCustomRandomPage = false
-$("input[name=config-enable-custom-startpage]").on("change", function() {
-  if ( $("input[name=config-enable-custom-startpage]").is(":checked") ) {
-    $("input[name=config-value-custom-startpage]").trigger("change")
-    UseCustomRandomPage = true
-  } else {
-    UseCustomRandomPage = false
-    Game.Settings.RandomPage = "https://en.wikipedia.org/wiki/Special:Random"
-  }
-})
-
-$("input[name=config-value-custom-startpage]").on("keyup change", function() {
-  if (UseCustomRandomPage) Game.Settings.RandomPage = $(this).val()
-})
 
 /**
  * Start a new game instance.
  */
-function GameStart() {
+exports.Start = function () {
 
   // Hide introduction section.
   $("section#intro").hide()
@@ -78,52 +86,56 @@ function GameStart() {
     },
     PreviousPage: null,
     TargetPage: null,
-    RandomPage: null,
     History: []
   }
 
   // Get target and random page from settings.
-  Game.Instance.RandomPage = Game.Settings.RandomPage
+  Game.Instance.StartPage.Page = Game.Settings.StartPage
 
   // Ask for target page.
   //Game.Instance.TargetPage = prompt("Enter full Wikipedia target URL:")
   Game.Instance.TargetPage = "https://en.wikipedia.org/wiki/Adolf_Hitler"
   if (Game.Instance.TargetPage == null) {
-    return GameStop()
+    return Game.Stop()
   }
   if (Game.Instance.TargetPage.indexOf("wikipedia.org/wiki/") === -1) {
-    return GameStart()
+    return Game.Start()
   }
 
   // Load random page!
   WikiPage = new BrowserWindow({show: false, fullscreenable: false})
-  WikiPage.loadURL(Game.Instance.RandomPage)
+  WikiPage.loadURL(Game.Instance.StartPage.Page)
 
   // Set statistics.
   Game.Instance.Clicks = "none"
 
   // Game is ready.
-  WikiPage.once("ready-to-show", GameReady)
+  WikiPage.once("ready-to-show", Game.PageReady)
 
   // We're navigating to a new page.
-  WikiPage.webContents.on("did-finish-load", GameReady)
+  WikiPage.webContents.on("did-finish-load", Game.PageReady)
 
   // Stop if wikipage is closed.
-  WikiPage.on("closed", GameStop)
+  WikiPage.on("closed", function () {
+    if (typeof WikiPage === "undefined") {
+      $("#game-over button#close").click()
+    }
+  })
 
 }
 
 /**
  * Prepare the game window once it's loaded.
  */
-function GameReady() {
+exports.PageReady = function () {
 
   WikiPage.show()
   console.log(WikiPage.webContents)
 
   // First page?
   if (Game.Instance.Clicks === "none") {
-    wikiStrip()
+
+    Game.StripWiki()
     // Set start page.
     console.log(WikiPage)
     Game.Instance.StartPage.Page = WikiPage.webContents.getURL()
@@ -135,11 +147,14 @@ function GameReady() {
     // Set first history item.
     Game.Instance.History[0] = Game.Instance.StartPage
     $(".game-data .history-data").append(Game.Instance.StartPage.Title+ " > ")
+
   } else {
+
     // Set previous page data.
     console.log('setting previous page data')
     Game.Instance.PreviousPage = Game.Instance.Data.Page
     console.log('previous page is ', Game.Instance.PreviousPage)
+
   }
 
   // Get title and page from document.
@@ -158,7 +173,7 @@ function GameReady() {
     $(".game-data .history-data").append(Game.Instance.Data.Title+ " > ")
 
     // Add image and styles to page.
-    wikiStrip()
+    Game.StripWiki()
 
   }
 
@@ -168,62 +183,75 @@ function GameReady() {
 
   // Check if we've reached the finish!
   if (Game.Instance.Data.Page == Game.Instance.TargetPage) {
+
     // Collect easy-to-read history.
     var NiceHistory = $(".game-data .history-data").text()
     // Save this game.
-    GameSave(Game.Instance.StartPage.Page, Game.Instance.TargetPage,
+    Game.Save(Game.Instance.StartPage.Page, Game.Instance.TargetPage,
              Game.Instance.Clicks, Game.Instance.History)
     // Update page statistics.
-    GameUpdateStats()
+    Game.UpdateStatistics()
     // Close WikiPage and do not show main sections.
-    GameStop()
+    setTimeout(Game.Stop(), 200)
     // Open the Game over section.
-    setTimeout(GameOver(Game.Instance, NiceHistory), 100)
+    setTimeout(Game.Over(Game.Instance, NiceHistory), 300)
+
   }
 
 }
+
 
 /**
  * Save the finsihed game
  * to storage.
  */
-function GameSave(StartPage, TargetPage, clicks, history) {
+exports.Save = function (StartPage, TargetPage, clicks, history) {
+
   var StartPage = StartPage.replace("https://en.wikipedia.org/wiki/", "")
   var TargetPage = TargetPage.replace("https://en.wikipedia.org/wiki/", "")
+
   var GameData = {
     "clicks": clicks,
     "StartPage": StartPage,
     "TargetPage": TargetPage,
     "history": history
   }
-  localStorage.setItem("WikiGame||"+new Date().getTime()+"||"+
-  StartPage+"||"+TargetPage, JSON.stringify(GameData))
+
+  localStorage.setItem(
+    "WikiGame||"+new Date().getTime()+"||"+
+    StartPage+"||"+TargetPage, JSON.stringify(GameData)
+  )
+
 }
+
 
 /**
  * Hide game elements and return to the main page.
  */
-function GameStop() {
+exports.Stop = function () {
   $("section#intro").show()
   $("section#stats").show()
   $("section#game").hide()
   $(".game-data .page-title").text("")
   $(".game-data .history-data").text("")
   $(".game-data .clicks").text("0")
+  console.log(remote)
   if (typeof(WikiPage) !== "undefined") WikiPage.close()
 }
+
 
 /**
  * Toggle configuration.
  */
-function ToggleConfig() {
+exports.ToggleConfig = function () {
   $("section#config").slideToggle(500)
 }
+
 
 /**
  * Update  the game statistics.
  */
-function GameUpdateStats() {
+exports.UpdateStatistics = function () {
 
   if (localStorage.length < 1) {
     $("#stats #games").html("<h4>Nothing to see here yet..</h4>")
@@ -248,10 +276,11 @@ function GameUpdateStats() {
   }
 }
 
+
 /**
  * Display game over message.
  */
-function GameOver(Game, NiceHistory) {
+exports.Over = function (Game, NiceHistory) {
   $("#game-over .clicks").text(Game.Clicks)
   $("#game-over .game-history").text(NiceHistory.replace(/ > $/, ''))
   $("section#game-over").show()
@@ -260,11 +289,12 @@ function GameOver(Game, NiceHistory) {
   $("body").attr("style", "overflow-y: hidden")
 }
 
+
 /**
  * Strip the wikipedia page of the main menu, footer, search bar and add
  * our own logo and clicks counter.
  */
-function wikiStrip() {
+exports.StripWiki = function () {
   const style  = 'text-align:center;text-transform:uppercase;'
   const execJS = 'l=document.getElementsByClassName("mw-wiki-logo")[0];'+
                  'l.setAttribute("style", "background-image:'+
